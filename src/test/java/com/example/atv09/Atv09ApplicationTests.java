aff0c9f5ef34d09d7d4a89e0125fa9af7619d5ab
Vitor Hugo <vferreira879@yahoo.com>
2024-04-30 20:49:06 -0300

    Inicial

---
+++ src/test/java/com/example/atv09/Atv09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atv09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atv09ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
